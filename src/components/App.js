b40a6afcc44f0b608c0c88d8ebd80753b0deac01
Roman nikolaenkov <rnikolaenkov@outlook.com>
2021-03-08 14:11:24 +0300

    ref: вынес компоненты из аппок

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../../App.css';
-import Header from '../Header/Header';
-import Main from "../Main/Main";
-import Footer from "../Footer/Footer";
+import '../App.css';
+import Header from './Header';
+import Main from "./Main";
+import Footer from "./Footer";
 
-import ImagePopup from "../ImagePopup/ImagePopup";
-import PopupWitForm from "../PopupWithForm/PopupWithForm";
+import ImagePopup from "./ImagePopup";
+import PopupWitForm from "./PopupWithForm";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
