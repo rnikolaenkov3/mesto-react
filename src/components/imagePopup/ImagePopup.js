5361118c2224ba90d400b97ff599dd98e0576dd9
Roman nikolaenkov <rnikolaenkov@outlook.com>
2021-03-08 13:45:19 +0300

    перевел проект на реакт

--- src/components/imagePopup/ImagePopup.js
+++ src/components/imagePopup/ImagePopup.js
@@ -1,21 +1,19 @@
 function ImagePopup(props) {
-    console.log(props);
-    let className = `popup popup_${props.name}`;
-    // if (props.isOpen) {
-    // if (true) {
-        className += ' popup_opened'
-    // }
+  let className = `popup popup_${props.name}`;
+  if (props.isOpen) {
+    className += ' popup_opened'
+  }
 
-    return (
-        <div className={className}>
-            <div className="popup__container popup__container_theme_photo">
-                <button type="button" className="popup__btn-close popup__btn-close_theme_photo root__link"
-                        aria-label="закрыть попап" onClick={props.onClose}></button>
-                <img src={props.card.link} alt={props.card.name} className="popup__photo" />
-                <h2 className="popup__title popup__title_theme_photo">{props.card.name}</h2>
-            </div>
-        </div>
-    );
+  return (
+    <div className={className}>
+      <div className="popup__container popup__container_theme_photo">
+        <button type="button" className="popup__btn-close popup__btn-close_theme_photo root__link"
+                aria-label="закрыть попап" onClick={props.onClose}></button>
+        <img src={props.card.link} alt={props.card.name} className="popup__photo"/>
+        <h2 className="popup__title popup__title_theme_photo">{props.card.name}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default ImagePopup;
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,74 +4,75 @@ import Card from "../card/Card";
 
 function Main(props) {
 
-    const [userName, setUserName] = React.useState('');
-    const [userDescription, setUserDescription] = React.useState('');
-    const [userAvatar, setUserAvatar] = React.useState('');
-    const [cards, setCards] = React.useState([]);
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+  const [cards, setCards] = React.useState([]);
 
-    const handleCardClick = (card) => {
-        props.onClick(card);
-    }
+  const handleCardClick = (card) => {
+    props.onCardClick(card);
+  }
 
-    React.useEffect(() => {
-       Api.getProfile()
-           .then((data) => {
-               setUserName(data.name);
-               setUserDescription(data.about);
-               setUserAvatar(data.avatar);
+  React.useEffect(() => {
+    Api.getProfile()
+      .then((data) => {
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
 
-           })
-           .catch((res) => {
-               console.log(res);
-           });
-       Api.getCardList()
-           .then((data) => {
-               setCards(data);
-           })
-           .catch((res) => {
-               console.log(res);
-           });
-    }, []);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+    Api.getCardList()
+      .then((data) => {
+        setCards(data);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  }, []);
 
-    return (
-        <main>
-            {/* profile */}
-            <section className="profile root__section">
-                <a href="#" className="profile__avatar-upload" target="_self" onClick={props.onEditAvatar}>
-                    <img src={userAvatar} alt="аватар" className="profile__avatar" />
-                </a>
-                <div className="profile__info">
-                    <div className="profile__info-wrap">
-                        <h1 className="profile__name">
-                            {userName}
-                        </h1>
-                        <button
-                            type="button"
-                            className="profile__btn-change root__link"
-                            aria-label="редактирование профиля"
-                            onClick={props.onEditProfile}
-                        >
-                        </button>
-                    </div>
-                    <p className="profile__role">
-                        {userDescription}
-                    </p>
-                </div>
-                <button type="button" className="profile__btn-add-img root__link" aria-label="добавление места" onClick={props.onAddPlace}></button>
-            </section>
-            {/* end profile */}
+  return (
+    <main>
+      {/* profile */}
+      <section className="profile root__section">
+        <a href="#" className="profile__avatar-upload" target="_self" onClick={props.onEditAvatar}>
+          <img src={userAvatar} alt="аватар" className="profile__avatar"/>
+        </a>
+        <div className="profile__info">
+          <div className="profile__info-wrap">
+            <h1 className="profile__name">
+              {userName}
+            </h1>
+            <button
+              type="button"
+              className="profile__btn-change root__link"
+              aria-label="редактирование профиля"
+              onClick={props.onEditProfile}
+            >
+            </button>
+          </div>
+          <p className="profile__role">
+            {userDescription}
+          </p>
+        </div>
+        <button type="button" className="profile__btn-add-img root__link" aria-label="добавление места"
+                onClick={props.onAddPlace}></button>
+      </section>
+      {/* end profile */}
 
-            {/* cards */}
-            <section className="cards root__section root__cards">
-                <ul className="places">
-                    {
-                        cards.map((card) => <Card card={card} key={card._id} onClick={handleCardClick}/>)
-                    }
-                </ul>
-            </section>
-            {/* end cards */}
-        </main>
-    )
+      {/* cards */}
+      <section className="cards root__section root__cards">
+        <ul className="places">
+          {
+            cards.map((card) => <Card card={card} key={card._id} onClick={handleCardClick}/>)
+          }
+        </ul>
+      </section>
+      {/* end cards */}
+    </main>
+  )
 }
 
 export default Main;
