60b934cc7bddd3e70b0eb2f2be56d34303936d28
Roman nikolaenkov <rnikolaenkov@outlook.com>
2021-03-21 13:30:20 +0300

    Использовал контекст CurrentUserContext в Main

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,6 +8,8 @@ import ImagePopup from "../imagePopup/ImagePopup";
 import PopupWithForm from "../popupWithForm/PopupWithForm";
 import Api from "../../utils/api";
 
+import {CurrentUserContext} from '../../context/CurrentUserContext';
+
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
@@ -60,82 +62,84 @@ function App() {
   }
 
   return (
-    <body className="root">
-    {/* header */}
-    <Header/>
-    {/* end header */}
-    <Main
-      onEditAvatar={handleEditAvatarClick}
-      onAddPlace={handleAddPlaceClick}
-      onEditProfile={handleEditProfileClick}
-      onCardClick={handleCardClick}
-    />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="root">
+        {/* header */}
+        <Header/>
+        {/* end header */}
+        <Main
+          onEditAvatar={handleEditAvatarClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditProfile={handleEditProfileClick}
+          onCardClick={handleCardClick}
+        />
 
-    {/* footer */}
-    <Footer/>
-    {/* footer end */}
+        {/* footer */}
+        <Footer/>
+        {/* footer end */}
 
-    <PopupWithForm
-      name='edit-profile'
-      title='Редактировать профиль'
-      isOpen={isEditProfilePopupOpen}
-      onClose={closeAllPopups}
-      buttonText='Сохранить'
-    >
-      <input type="text" className="popup__input popup__input_func_name" aria-label="имя" value=""
-             name="name"
-             id="profile-name" required minLength="2" maxLength="40" placeholder="Имя"/>
-      <span className="popup__error profile-name-error">Вы пропустили это поле</span>
-      <input type="text" className="popup__input popup__input_func_role" aria-label="роль" value=""
-             name="about" id="profile-role" required minLength="2" maxLength="200" placeholder="Роль"/>
-      <span className="popup__error profile-role-error">Вы пропустили это поле</span>
-    </PopupWithForm>
+        <PopupWithForm
+          name='edit-profile'
+          title='Редактировать профиль'
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          buttonText='Сохранить'
+        >
+          <input type="text" className="popup__input popup__input_func_name" aria-label="имя" value=""
+                 name="name"
+                 id="profile-name" required minLength="2" maxLength="40" placeholder="Имя"/>
+          <span className="popup__error profile-name-error">Вы пропустили это поле</span>
+          <input type="text" className="popup__input popup__input_func_role" aria-label="роль" value=""
+                 name="about" id="profile-role" required minLength="2" maxLength="200" placeholder="Роль"/>
+          <span className="popup__error profile-role-error">Вы пропустили это поле</span>
+        </PopupWithForm>
 
-    <PopupWithForm
-      name='add-card'
-      title='Новое место'
-      isOpen={isAddPlacePopupOpen}
-      onClose={closeAllPopups}
-      buttonText='Сохранить'
-    >
-      <input type="text" className="popup__input popup__input_name" aria-label="имя" value="" name="name"
-             placeholder="Название" id="new-card-name" required minLength="2" maxLength="30"/>
-      <span className="popup__error new-card-name-error">Вы пропустили это поле</span>
-      <input type="url" className="popup__input popup__input_link" aria-label="ссылка" value="" name="link"
-             placeholder="Ссылка на картинку" id="new-card-link" required/>
-      <span className="popup__error new-card-link-error">Вы пропустили это поле</span>
-    </PopupWithForm>
+        <PopupWithForm
+          name='add-card'
+          title='Новое место'
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          buttonText='Сохранить'
+        >
+          <input type="text" className="popup__input popup__input_name" aria-label="имя" value="" name="name"
+                 placeholder="Название" id="new-card-name" required minLength="2" maxLength="30"/>
+          <span className="popup__error new-card-name-error">Вы пропустили это поле</span>
+          <input type="url" className="popup__input popup__input_link" aria-label="ссылка" value="" name="link"
+                 placeholder="Ссылка на картинку" id="new-card-link" required/>
+          <span className="popup__error new-card-link-error">Вы пропустили это поле</span>
+        </PopupWithForm>
 
-    <PopupWithForm
-      name='upload-avatar'
-      title='Обновить аватар'
-      isOpen={isEditAvatarPopupOpen}
-      onClose={closeAllPopups}
-      buttonText='Сохранить'
-    >
-      <input type="url" className="popup__input popup__input_link" aria-label="ссылка" value="" name="link"
-             placeholder="Ссылка на картинку" id="avatar-upload-link" required/>
-      <span className="popup__error avatar-upload-link-error">Вы пропустили это поле</span>
-    </PopupWithForm>
+        <PopupWithForm
+          name='upload-avatar'
+          title='Обновить аватар'
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          buttonText='Сохранить'
+        >
+          <input type="url" className="popup__input popup__input_link" aria-label="ссылка" value="" name="link"
+                 placeholder="Ссылка на картинку" id="avatar-upload-link" required/>
+          <span className="popup__error avatar-upload-link-error">Вы пропустили это поле</span>
+        </PopupWithForm>
 
-    <PopupWithForm
-      name='delete-card'
-      title='Вы уверены?'
-      isOpen={isDeletePlacePopupOpen}
-      onClose={closeAllPopups}
-      buttonText='Сохранить'
-    >
-      <input type="hidden" className="popup__input popup__input_card-id" value="" name="card-id"/>
-    </PopupWithForm>
+        <PopupWithForm
+          name='delete-card'
+          title='Вы уверены?'
+          isOpen={isDeletePlacePopupOpen}
+          onClose={closeAllPopups}
+          buttonText='Сохранить'
+        >
+          <input type="hidden" className="popup__input popup__input_card-id" value="" name="card-id"/>
+        </PopupWithForm>
 
-    <ImagePopup
-      name='theme_photo'
-      card={selectedCard}
-      isOpen={isImagePopupOpen}
-      onClose={closeAllPopups}
-    />
-    {/* end modal */}
-    </body>
+        <ImagePopup
+          name='theme_photo'
+          card={selectedCard}
+          isOpen={isImagePopupOpen}
+          onClose={closeAllPopups}
+        />
+        {/* end modal */}
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,29 +1,20 @@
 import React from "react";
 import Api from "../../utils/api";
 import Card from "../card/Card";
+import {CurrentUserContext} from "../../context/CurrentUserContext";
 
 function Main(props) {
-
-  const [userName, setUserName] = React.useState('');
-  const [userDescription, setUserDescription] = React.useState('');
-  const [userAvatar, setUserAvatar] = React.useState('');
   const [cards, setCards] = React.useState([]);
+
+  const currentUser = React.useContext(CurrentUserContext);
+
+  console.log(currentUser);
 
   const handleCardClick = (card) => {
     props.onCardClick(card);
   }
 
   React.useEffect(() => {
-    Api.getProfile()
-      .then((data) => {
-        setUserName(data.name);
-        setUserDescription(data.about);
-        setUserAvatar(data.avatar);
-
-      })
-      .catch((res) => {
-        console.log(res);
-      });
     Api.getCardList()
       .then((data) => {
         setCards(data);
@@ -38,12 +29,12 @@ function Main(props) {
       {/* profile */}
       <section className="profile root__section">
         <a href="#" className="profile__avatar-upload" target="_self" onClick={props.onEditAvatar}>
-          <img src={userAvatar} alt="аватар" className="profile__avatar"/>
+          <img src={currentUser.avatar} alt="аватар" className="profile__avatar"/>
         </a>
         <div className="profile__info">
           <div className="profile__info-wrap">
             <h1 className="profile__name">
-              {userName}
+              {currentUser.name}
             </h1>
             <button
               type="button"
@@ -54,7 +45,7 @@ function Main(props) {
             </button>
           </div>
           <p className="profile__role">
-            {userDescription}
+            {currentUser.about}
           </p>
         </div>
         <button type="button" className="profile__btn-add-img root__link" aria-label="добавление места"
