fafbcc01f380b38f7df558e480701541297e6805
Roman nikolaenkov <rnikolaenkov@outlook.com>
2021-03-22 12:07:27 +0300

    покахываем лайкнули ли мы карточку

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,27 +3,26 @@ import {CurrentUserContext} from "../../context/CurrentUserContext";
 
 function Card(props) {
   const currentUser = React.useContext(CurrentUserContext);
-
-  // Определяем, являемся ли мы владельцем текущей карточки
-  const isOwn = props.card.owner._id === currentUser._id;
-
-  console.log(`Card: ${props.card._id} - is own: ${isOwn}`);
-
   const handleClick = () => {
     props.onClick(props.card);
   }
 
-  let btRemoveClassName = 'places__remove root__link';
-  if (isOwn) { btRemoveClassName += ' places__remove_visible'}
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = props.card.owner._id === currentUser._id;
+  const cardDeleteBtnClassName = (`places__remove root__link ${isOwn && 'places__remove_visible'}`);
+  //лайкнули ли карточку
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = `places__like root__link ${isLiked && 'places__like_active'}`;
+
 
   return (
     <li className="places__card">
-      <button type="button" className={btRemoveClassName}></button>
+      <button type="button" className={cardDeleteBtnClassName}></button>
       <img src={props.card.link} alt={props.card.name} className="places__img" onClick={handleClick}/>
       <div className="places__title-wrap">
         <h2 className="places__title">{props.card.name}</h2>
         <div className="places__like-wrap">
-          <button type="button" className="places__like root__link"></button>
+          <button type="button" className={cardLikeButtonClassName}></button>
           <p className="places__like-count">{props.card.likes.length}</p>
         </div>
       </div>
